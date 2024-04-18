7d18c9f8540cc0c2139814e3dc4eceef17ca9b4d
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-04-18 12:13:05 +0300

    Initial Commit

---
+++ SimpleReader-12-15-SD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleReader-12-15-SD
+//
+//  Created by Andrei Shpartou on 18/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
