a8a953f9057333add839a7262d13bf3cca08b7a1
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-04-24 21:12:24 +0300

    complete stopwatch

--- SimpleReader-12-15-SD/Scenes/ButtonConfiguration/Controller/ButtonConfigurationViewController.swift
+++ SimpleReader-12-15-SD/Scenes/ButtonConfiguration/Controller/ButtonConfigurationViewController.swift
@@ -7,15 +7,71 @@
 
 import UIKit
 
-class ButtonConfigurationViewController: UIViewController {
+protocol ButtonConfigViewControllerDelegate: AnyObject {
+    func secondsChanged(_ seconds: Int)
+}
+
+class ButtonConfigurationViewController: UIViewController, ButtonViewDelegate {
     weak var coordinator: MainCoordinator?
+    weak var delegate: ButtonConfigViewControllerDelegate?
 
     private var buttonConfigurationView: ButtonConfigurationView?
+    private var stopWatch: Stopwatch?
 
     // MARK: - LifeCycle
     override func loadView() {
         buttonConfigurationView = ButtonConfigurationView()
+        buttonConfigurationView?.delegate = self
         view = buttonConfigurationView
+        delegate = buttonConfigurationView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        stopWatch = Stopwatch()
+    }
+}
+
+// MARK: - Action methods
+private extension ButtonConfigurationViewController {
+    @objc
+    private func secondsChanged(_ notification: NSNotification) {
+        guard let userInfo = notification.userInfo,
+              let seconds = userInfo["seconds"] as? Int else {
+            return
+        }
+
+        delegate?.secondsChanged(seconds)
+    }
+}
+
+// MARK: - ButtonViewDelegate
+extension ButtonConfigurationViewController {
+
+    func start() {
+        stopWatch?.start()
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(secondsChanged),
+            name: .secondsChanged,
+            object: nil
+        )
+    }
+
+    func pause() {
+        stopWatch?.pause()
+    }
+
+    func stop() {
+        stopWatch?.stop()
+
+        NotificationCenter.default.removeObserver(
+            self,
+            name: .secondsChanged,
+            object: nil
+        )
     }
 }
 
--- SimpleReader-12-15-SD/Scenes/ButtonConfiguration/View/ButtonConfigurationView.swift
+++ SimpleReader-12-15-SD/Scenes/ButtonConfiguration/View/ButtonConfigurationView.swift
@@ -10,11 +10,22 @@ import UIKit
 protocol ButtonConfigurationProtocol {
 }
 
+protocol ButtonViewDelegate: AnyObject {
+    func start()
+    func pause()
+    func stop()
+}
+
 class ButtonConfigurationView: UIView, ButtonConfigurationProtocol {
+    weak var delegate: ButtonViewDelegate?
+
+    // MARK: - View model Properties
+    private var isStopWatchActive = false
+
     // MARK: - View Model Properties
     private var timeString: String? {
         didSet {
-            startButton.setNeedsUpdateConfiguration()
+            startButton.configuration?.subtitle = timeString
         }
     }
     // MARK: - View Properties
@@ -58,7 +69,12 @@ class ButtonConfigurationView: UIView, ButtonConfigurationProtocol {
     private lazy var stopButton = UIButton.getDefaultFilledButton(
         title: "Stop",
         isDefaultImageConfig: true,
-        action: nil
+        action: UIAction(handler: { _ in
+            self.delegate?.stop()
+            if self.startButton.isSelected {
+                self.startButton.isSelected = false
+            }
+        })
     )
 
     // MARK: - Init
@@ -92,24 +108,45 @@ class ButtonConfigurationView: UIView, ButtonConfigurationProtocol {
     -> (_ button: UIButton) -> Void {
         return { [unowned self] button in
             var config = button.configuration
-            let isSelected = button.isSelected
 
-            if isSelected {
-                config?.image = UIImage(systemName: "pause.fill")
-                config?.title = "Pause"
-                config?.subtitle = timeString
-                config?.titlePadding = Sizes.Small.padding
-                config?.showsActivityIndicator = true
-            } else {
+            switch button.state {
+            case .normal:
                 config?.image = UIImage(systemName: "play.fill")
-                config?.title = "Play"
+                config?.attributedTitle = AttributedString(
+                    "Play",
+                    attributes: AttributeContainer([NSAttributedString.Key.font: UIFont.appLargeBold])
+                )
                 config?.subtitle = nil
                 config?.titlePadding = 0
                 config?.showsActivityIndicator = false
+                button.configuration = config
+                delegate?.pause()
+            case .selected:
+                config?.image = UIImage(systemName: "pause.fill")
+                config?.attributedTitle = AttributedString(
+                    "Pause",
+                    attributes: AttributeContainer([NSAttributedString.Key.font: UIFont.appLargeBold])
+                )
+                config?.subtitle = timeString
+                config?.titlePadding = Sizes.Small.padding
+                config?.showsActivityIndicator = true
+                button.configuration = config
+                delegate?.start()
+            default:
+                return
             }
-
-            button.configuration = config
         }
+    }
+}
+
+// MARK: - ButtonConfigViewControllerDelegate
+extension ButtonConfigurationView: ButtonConfigViewControllerDelegate {
+    func secondsChanged(_ seconds: Int) {
+        let hours = seconds / 3600
+        let minutes = (seconds % 3600) / 60
+        let seconds = (seconds % 3600) % 60
+        timeString = String(format: "%02d : %02d : %02d", hours, minutes, seconds)
+        stopWatchLabel.text = timeString
     }
 }
 
